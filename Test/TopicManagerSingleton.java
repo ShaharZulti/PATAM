cc3e106bbae4175e2667d7f493421515a74897e3
ShaharZulti <165648995+ShaharZulti@users.noreply.github.com>
2026-01-22 17:23:07 +0200

    Implement singleton pattern in TopicManagerSingleton
    Refactor TopicManagerSingleton to implement singleton pattern with topic management functionality.

--- Test/TopicManagerSingleton.java
+++ Test/TopicManagerSingleton.java
@@ -1,10 +1,32 @@
 package test;
 
+import java.util.Collection;
+import java.util.HashMap;
 
 public class TopicManagerSingleton {
 
     public static class TopicManager{
-    }
+        private static final TopicManager instance = new TopicManager();
+        private final HashMap<String, Topic> topics = new HashMap<String, Topic>();
 
+        private TopicManager() {
+        }
+
+        public Topic getTopic(String name){
+            return this.topics.computeIfAbsent(name, Topic::new);
+        }
+
+        public Collection<Topic> getTopics(){
+            return this.topics.values();
+        }
+
+        public void clear(){
+            this.topics.clear();
+        }
+
+    }
+    public static TopicManager get(){
+        return TopicManager.instance;
+    }
     
 }
